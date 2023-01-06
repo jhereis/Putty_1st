79cee49e5736ff199ccb7230fb10926ea7a2aef3
jhenn reis <101225404+jhereis@users.noreply.github.com>
2023-01-06 03:22:01 +0100

    Add files via upload

---
+++ arquivo.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void main() {
+	float a, b, c;
+	printf("\nInforme um numero: ");
+	scanf("%f", &a);
+
+if(a>10)
+{
+	printf("\nValor digitado é maior que 10");
+}
+else
+{
+	printf("\nValor digitado é menor ou igual a 10\n");
+}
+}
---
+++ programa.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void){	
+	printf("Boa tarde");
+	printf("\n\n");
+	return 0;
+	}
